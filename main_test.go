3d3e83eacf2471e6c86915e4aa1b3c7412fc0a05
nothingrealm <shafaghi1378@gmail.com>
2020-04-07 01:18:01 +0430

    Test added

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServerHTTP(t *testing.T) {
+	handler := &Server{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatal("Received non-200")
+	}
+	expected := `{"message": "hello world"}`
+	actual, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != string(actual) {
+		t.Errorf("Wrong message")
+	}
+}
